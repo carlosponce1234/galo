bbaf85c2b8c1e38074f467bdf6563ab2dece6c76
carlosponce1234 <40065410+carlosponce1234@users.noreply.github.com>
2019-04-01 11:26:32 -0600

    favicon añadido

--- views/soporte.php
+++ views/soporte.php
@@ -60,10 +60,7 @@ $headers .= 'From: <not-reply@documentosgaloyasociados.com>' . "\r\n";
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="apple-touch-icon" sizes="180x180" href="/galo/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/galo/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/galo/favicon-16x16.png">
-<link rel="manifest" href="/galo/site.webmanifest">
+    <link rel='icon' href='../favicon.ico' type='image/x-icon'/ >
     <title>Login</title>
     <link rel="stylesheet" href="../css/foundation.css">
     <link rel="stylesheet" href="../css/app.css">
